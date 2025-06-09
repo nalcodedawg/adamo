647e074eaa19e0ba0874b77d63c17900f4fb3268
nalcodedawg <128307338+nalcodedawg@users.noreply.github.com>
2025-06-09 22:55:47 +0300

    Update [code].tsx

--- pages/room/[code].tsx
+++ pages/room/[code].tsx
@@ -1,1 +1,9 @@
+export default function RoomPage() {
+  return (
+    <div>
+      <h1>Adamo World Room</h1>
+      <p>This is the group chat page for Adamo World.</p>
+    </div>
+  );
+}
 // Adamo World Room Chat Page

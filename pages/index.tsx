85f02691486bb43a6289a199cc31c37af2220890
nalcodedawg <128307338+nalcodedawg@users.noreply.github.com>
2025-06-10 13:20:19 +0300

    Delete pages directory

--- pages/index.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div>
-      <h1>Welcome to Adamo Chat</h1>
-      <p>This is your landing page. Login or explore rooms.</p>
-    </div>
-  );
-}
--- pages/room/[code].tsx
+++
@@ -1,9 +0,0 @@
-export default function RoomPage() {
-  return (
-    <div>
-      <h1>Adamo World Room</h1>
-      <p>This is the group chat page for Adamo World.</p>
-    </div>
-  );
-}
-// Adamo World Room Chat Page
